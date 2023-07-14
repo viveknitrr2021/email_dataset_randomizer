2e8738b27007ab26c0499afddc93f1744eeb0635
VIVEK KUMAR SINGH <vks042.rkt@gmail.com>
2023-07-14 14:58:45 +0530

    New Changes

--- com/FLASK_APP/controller.py
+++ com/FLASK_APP/controller.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from app import predict
 
 app = Flask(__name__)
 
@@ -6,6 +7,10 @@ app = Flask(__name__)
 def welcome():
     return "Health Check Successful"
 
+@app.route('/check', methods=['GET', 'POST'])
+def check():
+    return predict("Confirmation Details Trade affirmation Summary Report Trade Confirmation Affirmation of 8991889924 LVL see the following attachment Affirmation statement.json")
 
 if __name__ == '__main__':
     app.run()
+
---
+++ com/FLASK_APP/first.py
@@ -0,0 +1,5 @@
+from app import predict
+
+print(50*"**")
+print(predict("Confirmation Details Trade affirmation Summary Report Trade Confirmation Affirmation of 8991889924 LVL see the following attachment Affirmation statement.json"))
+print(50*"**")
